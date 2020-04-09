42b27278c9a91191938758eb640973067d736b20
ZJRDroid <priv.zjr@gmail.com>
2020-04-09 11:05:11 -0300

    Move fingerprint back to init

--- init/init_potter.cpp
+++ init/init_potter.cpp
@@ -48,6 +48,13 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 /* Get Ram size for different variants */
 void check_device()
 {
@@ -82,6 +89,10 @@ void vendor_load_properties()
     sku.append(android::base::GetProperty("ro.boot.hardware.sku", ""));
     sku.append(")");
     property_set("ro.product.model", sku.c_str());
+
+    // fingerprint
+    property_override("ro.build.description", "potter-user 8.1.0 OPSS28.85-17-4 28698 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "motorola/payton/payton:8.0.0/OPWS27.57-25-6-10/12:user/release-keys");
 
     // rmt_storage
     std::string device = android::base::GetProperty("ro.boot.device", "");
